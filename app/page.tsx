60dff496f77a0918f6633023b4d6ea32ae433ed8
sargon17 <63978197+sargon17@users.noreply.github.com>
2023-06-20 15:48:37 +0200

    experimenting with animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useRef, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
-import { inView, scrolledTL } from "@/lib/anima-scroll";
+import { inView, TL } from "@/lib/anima-scroll";
 
 import Rows from "@/components/utils/rows";
 
@@ -10,7 +10,11 @@ export default function Home() {
   const container = useRef(null);
 
   const [isInView, setIsInView] = useState(false);
-  const [scrolled, setScrolled] = useState("");
+  const [entered, setEntered] = useState(0 as number);
+  const [exited, setExited] = useState(0 as number);
+  const [fullMotion, setFullMotion] = useState(0 as number);
+
+  const tl = new TL({ ref: container });
 
   window.addEventListener("scroll", () => {
     const containerInView = inView({ ref: container });
@@ -19,10 +23,18 @@ export default function Home() {
     }
 
     if (containerInView) {
-      const scrolled = scrolledTL({ ref: container });
-      setScrolled(scrolled);
+      setEntered(tl.entering());
+      setExited(tl.exiting());
+      setFullMotion(tl.fullMotion());
     }
   });
+
+  const containerClass = () => {
+    let base =
+      "border-2 border-black rounded-2xl shadow-xl w-full relative overflow-hidden relative my-12 h-[600px] ";
+
+    return base;
+  };
 
   return (
     <main className="flex min-h-screen flex-col p-24">
@@ -43,17 +55,24 @@ export default function Home() {
       </div>
       <div
         ref={container}
-        className=" border-2 border-black rounded-2xl shadow-xl w-full relative overflow-hidden relative mb-4"
+        className={containerClass()}
+        style={{
+          transform: `translateY(-${fullMotion / 4}%)`,
+          height: `${600 - fullMotion * 0.2}px`,
+        }}
       >
         <Image
           src="/image.jpg"
           alt="hero"
           width={1974}
           height={1316}
-          className="object-cover "
+          className="object-cover relative z-10"
+          style={{
+            transform: `translateY(-${fullMotion / 10}%) `,
+          }}
         />
 
-        <Rows />
+        {/* <Rows /> */}
       </div>
       <div className=" border-2 border-black rounded-2xl shadow-xl w-full relative overflow-hidden relative mb-4">
         <Image
@@ -64,13 +83,20 @@ export default function Home() {
           className="object-cover "
         />
       </div>
+
       {/* Data Screen */}
       <div className="fixed top-4 right-4 px-6 py-4 bg-white  rounded-xl w-48 z-50  border shadow-2xl">
         <p>
           is in view: <span id="data">{isInView ? "true" : "false"}</span>
         </p>
         <p>
-          scrolled: <span id="data">{scrolled ? scrolled : "0"}%</span>
+          Entered: <span id="data">{entered ? entered : "0"}%</span>
+        </p>
+        <p>
+          Exited: <span id="data">{exited ? exited : "0"}%</span>
+        </p>
+        <p>
+          Full Motion: <span id="data">{fullMotion ? fullMotion : "0"}%</span>
         </p>
       </div>
     </main>
